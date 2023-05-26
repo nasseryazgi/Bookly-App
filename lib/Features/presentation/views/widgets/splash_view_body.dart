3fc9dd78e5ee28ef14266314b62bc0f2c17a92a9
nasser <nas@gmail.com>
2023-05-26 19:05:59 +0300

    Adding sliding animation to splash_view_body.dart

--- lib/Features/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/presentation/views/widgets/splash_view_body.dart
@@ -1,9 +1,35 @@
 import 'package:flutter/cupertino.dart';
 
 import '../../../../core/utils/assets.dart';
+import 'sliding_text.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({Key? key}) : super(key: key);
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    super.initState();
+
+    // TODO: implement initState
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(minutes: 1));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(2, 0), end: Offset.zero)
+            .animate(animationController);
+    slidingAnimation.addListener(() {
+      setState(() {});
+    });
+
+    dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,10 +41,7 @@ class SplashViewBody extends StatelessWidget {
         const SizedBox(
           height: 10,
         ),
-        const Text(
-          'Read Free Books',
-          textAlign: TextAlign.center,
-        )
+        SlidingText(slidingAnimation: slidingAnimation)
       ],
     );
   }
