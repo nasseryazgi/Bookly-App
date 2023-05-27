c55139bc8bd97b1db2ecb307bd84a5a98984990e
nasser <nas@gmail.com>
2023-05-27 20:31:04 +0300

    create custom app bar

---
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../../../../core/utils/assets.dart';
+import 'custom_app_bar.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [CustomAppBar()],
+    );
+  }
+}
