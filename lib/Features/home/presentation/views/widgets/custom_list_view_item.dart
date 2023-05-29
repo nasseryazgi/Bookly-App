0d72c5566649ad3bbef1114e602f2bea948f7068
nasser <nas@gmail.com>
2023-05-29 14:48:49 +0300

    add FeaturedBooksListView to home

--- lib/Features/home/presentation/views/widgets/custom_list_view_item.dart
+++ lib/Features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -6,19 +6,16 @@ class CustomListViewItem extends StatelessWidget {
   const CustomListViewItem({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.height * .3,
-      child: AspectRatio(
-        aspectRatio: 2.7 / 4,
-        child: Container(
-          height: 100,
-          //  width: MediaQuery.of(context).size.height * .25,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16),
-              //color: Colors.black,
-              image: const DecorationImage(
-                  fit: BoxFit.fill, image: AssetImage(AassetsData.testImage))),
-        ),
+    return AspectRatio(
+      aspectRatio: 2.7 / 4,
+      child: Container(
+        height: 100,
+        //  width: MediaQuery.of(context).size.height * .25,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            //color: Colors.black,
+            image: const DecorationImage(
+                fit: BoxFit.fill, image: AssetImage(AassetsData.testImage))),
       ),
     );
   }
--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -3,14 +3,15 @@ import 'package:flutter/material.dart';
 
 import 'custom_app_bar.dart';
 import 'custom_list_view_item.dart';
+import 'featured_books_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return const Column(
-      children: [CustomAppBar(), CustomListViewItem()],
+      children: [CustomAppBar(), FeaturedBooksListView()],
     );
   }
 }
+
