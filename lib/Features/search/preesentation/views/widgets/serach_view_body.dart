e7c9ce02286cbd76e3c1098283e41972461e3d11
nasser <nas@gmail.com>
2023-07-03 01:22:53 +0300

    search view ui done

--- lib/Features/search/preesentation/views/widgets/serach_view_body.dart
+++ lib/Features/search/preesentation/views/widgets/serach_view_body.dart
@@ -1,16 +1,54 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../../../../../core/utils/styles.dart';
+import '../../../../home/presentation/views/widgets/best_saller_listview_item.dart';
 import 'custom_search_text.dart';
+
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Column(
-        children: [CustomSearchTextField()],
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CustomSearchTextField(),
+            SizedBox(
+              height: 16,
+            ),
+            Text(
+              'Search Result',
+              style: Styles.textStyle18,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Expanded(child: SearchResultView())
+          ],
+        ),
       ),
     );
   }
 }
 
+class SearchResultView extends StatelessWidget {
+  const SearchResultView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      physics: const NeverScrollableScrollPhysics(),
+      padding: EdgeInsets.zero,
+      itemCount: 9,
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: EdgeInsets.symmetric(vertical: 10.0),
+          child: BestSellerListViewItem(),
+        );
+      },
+    );
+  }
+}
