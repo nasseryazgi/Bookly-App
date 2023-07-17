28e38df31443fd0ec9e8d1b7b2ed50f40d4b1532
nasseryazgi <naserkoker.xp@gmail.com>
2023-07-17 03:05:14 +0300

    Create featured books cubit states

---
+++ lib/Features/home/data/presentation/mananger/featured_books_cubit/featured_books_cubit.dart
@@ -0,0 +1,6 @@
+import 'package:bookly/Features/home/data/presentation/mananger/featured_books_cubit/featured_books_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
+  FeaturedBooksCubit(super.initialState);
+}
