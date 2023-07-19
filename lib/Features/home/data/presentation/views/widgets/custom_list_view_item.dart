bf0b4b2514e561cd81e73160e11d3aa26ee84665
nasseryazgi <naserkoker.xp@gmail.com>
2023-07-19 12:46:42 +0300

    Add Cached network image

--- lib/Features/home/data/presentation/views/widgets/custom_list_view_item.dart
+++ lib/Features/home/data/presentation/views/widgets/custom_list_view_item.dart
@@ -1,23 +1,34 @@
-import 'package:flutter/cupertino.dart';
-
-import '../../../../../../core/utils/assets.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
 
 class CustomBookImage extends StatelessWidget {
   const CustomBookImage({Key? key, required this.imageUrl}) : super(key: key);
-  @override
   final String imageUrl;
   Widget build(BuildContext context) {
     return AspectRatio(
       aspectRatio: 2.6 / 4,
-      child: Container(
+      child: CachedNetworkImage(
+        imageUrl: imageUrl,
+        fit: BoxFit.fill,
+        placeholder: (context, url) => const Center(
+          child: CircularProgressIndicator(),
+        ),
+        errorWidget: (context, url, error) => const Icon(Icons.error),
+      ),
+    );
+  }
+}
+
+
+/*
+  child: Container(
         height: 100,
         //  width: MediaQuery.of(context).size.height * .25,
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(16),
             //color: Colors.black,
-            image: DecorationImage(
-                fit: BoxFit.fill, image: NetworkImage(imageUrl))),
+            image:  DecorationImage(
+                fit: BoxFit.fill, 
+                image: CachedNetworkImageProvider())),
       ),
-    );
-  }
-}
+*/
